004da2ba8968dbf625819f3f2460fed407ff2cc3
ivij <53103290+ivij@users.noreply.github.com>
2019-09-11 00:59:44 +1000

    Add files via upload

---
+++ _ishan_bottle_drawing.dwg.18108.tmp.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+Void loop() 
+{ 
+If(motion detected) 
+{ 
+digitalWrite(LED_BUILTIN, HIGH); 
+} 
+Else 
+{ 
+  digitalWrite(LED_BUILTIN, LOW);  
+} 
+Delay(3000); 
